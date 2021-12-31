5592ace7d61d606d58ab5708ddbc39d4e9d512d9
Sieroslawski <adrian.sieroslawski@gmail.com>
2021-12-31 13:41:35 -0800

    CSS and minor details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import logo from '../images/movie-projector.png'
+
 function Footer() {
     return (
         //A simple footer
         <footer>           
-            <p class="name">&copy; Adrian Sieroslawski - SSD - 2021</p>
+            <p class="name" >&copy; Adrian Sieroslawski - SSD - 2021 <img src={logo} alt="movie-projector-logo" className="logo-footer"/></p>
         </footer>
     )
 }
--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -3,16 +3,23 @@ import Logo from '../images/tmdb-logo.svg'
 function PageAbout() {
     return (
         // Display a simple about page
-        <section className="about-page">
-            <h2>Welcome to MVDB!</h2>
-            <h3>About this project</h3>
-            <p>MVDB is a Movie Database listing the movies based on popularity, rating, and release date. 
-                Browse for your favourite film, add it to the Favourite List, and save it for the Watch Later list!</p>
-            <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
-            <div class="tmdb-logo">
-            <img src={Logo} alt="TMDB-Logo" />
+    <div className="about-page-wrapper">
+        <section className="about-project">
+            <h2 className="about-moviemania">Welcome to Movie Mania!</h2>
+            <h3 className="about-title">About this project</h3>
+            <p>Movie Mania is a Movie Database listing the movies based on popularity, rating, and release date. 
+                Browse for your favourite film, add it to the Favourite List!</p>
+        </section>
+        <section className="about-ipsum">
+            <h3 className="about-title">Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="not-endorsed">This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+            <div className="tmdb-logo">
+            <img src={Logo} alt="TMDB-Logo" id="logo"/>
             </div>
         </section>
+    </div>
     )
 }
 
